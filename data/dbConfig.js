183d8a0113e9d34ed8004ee40c2a3c0431a17110
Anthony <anthony.kizer88@gmail.com>
2019-10-24 22:48:36 -0700

    added tests to auth route

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,5 +2,6 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile.js");
 
-const dbEnv = process.env.DB_ENV || "development";
-module.exports = knex(knexConfig[dbEnv]);
+// const dbEnv = process.env.DB_ENV || "development";
+// module.exports = knex(knexConfig[dbEnv]);
+module.exports = knex(knexConfig.development);
